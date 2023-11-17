f3efa64445f911def55b1f15c8b7448a6a176cb6
Luan Rodrigues <luancostar@gmail.com>
2023-11-17 18:15:06 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow';
+
+ export default () => {
+
+    const [movieList, setMovieList] = useState([]);
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+ 
+
+  return (
+    <div className="page">
+        <section className="lists">
+          {movieList.map((item, key)=>(
+            <MovieRow key={key} />
+          ))}
+        </section>
+    </div>
+  );
+  }
+
---
+++ src/components/MovieRow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './MovieRow.css';
+
+export default ({title, items}) => {
+    return (
+        <div>
+            Lista do Filme
+        </div>
+    );
+}
+
