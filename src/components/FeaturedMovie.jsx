c193bdfd35808bb79c02ec6ea764cabda1f28c1f
luan <luancostar@gmail.com>
2023-11-19 18:25:31 -0300

    atualizacao de integracao

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import './FeaturedMovie.css';
 
 export default ({item}) => {
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+
+    for ( let i in item.genres) {
+        genres.push( item.genres[i].name );
+    }
+
+    
     return (
        <section className="featured" style={{
             backgroundSize: 'cover',
@@ -13,15 +22,21 @@ export default ({item}) => {
                 <div className="featured--name">
                     {item.original_name}
                 </div>
-                <div className="feaatured--info">
+                <div className="featured--info">
                     <div className="featured--points">
                         {item.vote_average}
                         pontos
                     </div>
-                    <div className="featured-years">2099</div>
-                    <div className="featured-seasons">{item.number_of_seasons} temporada {item.number_of_seasons !==1 ? 's' : ''}</div>
+                    <div className="featured--years">{firstDate.getFullYear()}</div>
+                    <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !==1 ? 's' : ''}</div>
                 </div>
+                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--buttons">
+                        <a className="featured--watchbutton" href="{`/watch/${item.id}`}">► Assistir</a>
+                        <a className="featured--mylistbutton" href="">+ Minha Lista</a>
 
+                    </div>
+                    <div className="featured--genres"><strong>Gêneros: </strong>{genres.join(', ')}</div>
               </div>
         </div>
        </section>
