92af362e3ee36de970d49bff953152776be78c86
luan <luancostar@gmail.com>
2023-11-19 17:32:43 -0300

    atualizacao de integracao

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import './FeaturedMovie.css';
 
-export default () => {
+export default ({item}) => {
     return (
-        <div>
-            FILME EM DESTAQUE
+       <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+       }}>    
+        <div className="featured--vertical">
+               <div className="featured--horizontal">
+                <div className="featured--name">
+                    {item.original_name}
+                </div>
+                <div className="feaatured--info">
+                    <div className="featured--points">
+                        {item.vote_average}
+                        pontos
+                    </div>
+                    <div className="featured-years">2099</div>
+                    <div className="featured-seasons">{item.number_of_seasons} temporadas</div>
+                </div>
+
+              </div>
         </div>
+       </section>
     );
 }
